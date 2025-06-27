03bd097329f25b2d7a33256aa2e06663b3c1705c
Mr-Aceil <mimiracle022@gmail.com>
2025-06-27 09:48:12 +0100

    Img bg backup

--- src/Home_page/section3cards.jsx
+++ src/Home_page/section3cards.jsx
@@ -4,7 +4,7 @@ import Image from '../assets/projects/44DB.png'
 export default function section3cards({image=Image, name='Project name', description='Write a description'}) {
     return (
         <div className='bg-borderColor rounded-[10px] p-[10px] w-full gap-[15px] grow-[1] flex flex-col items-center'>
-            <img src={image} alt=""  className='rounded-[10px]'/>
+            <img src={image} alt=""  className='rounded-[10px] bg-imgbackup'/>
 
             <div className='flex flex-col gap-[5px] w-full'>
                 <p className='text-mainTetx'>{name}</p>
