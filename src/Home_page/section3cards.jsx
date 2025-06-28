366c2bae734e109e16ad1cf92ee7e2f3e485dad5
Mr-Aceil <mimiracle022@gmail.com>
2025-06-28 20:53:34 +0100

    Updated

--- src/Home_page/section3cards.jsx
+++ src/Home_page/section3cards.jsx
@@ -3,7 +3,7 @@ import Image from '../assets/projects/44DB.png'
 
 export default function section3cards({image=Image, name='Project name', description='Write a description'}) {
     return (
-        <div className='bg-borderColor rounded-cardround p-[5px] pb-[20px] w-full gap-[15px] grow-[1] flex flex-col items-center'>
+        <div className='bg-borderColor rounded-cardround p-[5px] pb-[25px] w-full gap-[15px] grow-[1] flex flex-col items-center'>
             <img src={image} alt=""  className='rounded-cardround bg-imgbackup'/>
 
             <div className='flex flex-col gap-[5px] w-full'>
