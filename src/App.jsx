b1a6e81d19d5b6be185b4c4346483e96c49f4ca7
Mr-Aceil <mimiracle022@gmail.com>
2025-07-02 21:18:22 +0100

    Font and padding updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Header from './Home_page/Header'
 import Section1 from './Home_page/section1'
 import Section2 from './Home_page/section2'
 import Section3 from './Home_page/section3'
+import Section4 from './Home_page/section4'
+
 
 export default function App() {
   return (
@@ -11,6 +13,7 @@ export default function App() {
     <Section1/>
     <Section2/>    
     <Section3/>    
+    <Section4/>    
     </>
   )
 }
---
+++ src/Home_page/section4.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function section4() {
+    return (
+        <section className='text-mainText outside flex-col'>
+            <div className='outside inside flex-col inp3'>
+                <h2 className='text-h2 text-center'>Three steps we follow.</h2>
+            </div>
+        </section>
+    )
+}
